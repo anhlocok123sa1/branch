3989cdc33af5c438104bdb1b212a324021206cfe
Son <duyson68@gmail.com>
2023-10-30 13:51:52 +0700

    thuong

--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,9 @@ using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
-float thuong(int a, int b);
+float thuong(int a, int b){
+	return a/b;
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
